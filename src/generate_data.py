24fc7df2d08cf0d5c5ddb14f5bc8a462dcffc102
Yogrshwari Sukdeo Patil <yogeshwari.patil2006@gmail.com>
2026-09-10 14:15:38 +0530

    docs; add all docs

---
+++ src/generate_data.py
@@ -0,0 +1,6 @@
+from sklearn.datasets import load_iris 
+ 
+iris = load_iris(as_frame=True) 
+df = iris.frame 
+df.to_csv("data/raw/iris_v1.csv", index=False) 
+print(f"Saved {len(df)} rows to data/raw/iris_v1.csv") 
